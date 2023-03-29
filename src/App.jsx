f2800dc95df56f5c1381da7da137b453a62ecfe5
Potato <poltoradnevone@gmail.com>
2023-03-29 08:15:13 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => (
+  <button onClick={() => console.log("clicked")}>Click me</button>
+);
+
+export default Button;
